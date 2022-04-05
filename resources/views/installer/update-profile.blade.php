a6f68fefeb8b34f8f918ddf6fbe41b1f8bc898b5
godlikeAlex <godlikedesigner@gmail.com>
2022-04-05 22:02:25 +0500

    added view for update profile

---
+++ resources/views/installer/update-profile.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app-installer')
+
+@section('content')
+<div class="card" style="background: #f7f7f7;">
+    <!-- IT IS REACT COMPONENT! IN RESOURCE FOLDER ALL COMPONENTS -->
+    <div id="update-installer-profile" data-user="{{$user}}"></div>
+</div>
+
+<script>
+
+</script>
+@endsection
