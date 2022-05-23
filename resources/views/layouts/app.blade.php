79ba936f5bf4d0857b815f467ca81c9aa4d9495c
godlikeAlex <godlikedesigner@gmail.com>
2022-05-23 20:01:30 +0500

    Added Google Tag Manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,13 @@
     <!-- Global site tag (gtag.js) - Google Ads: 10866148222 --> 
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10866148222"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-10866148222'); </script> 
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KH6MS3L');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
